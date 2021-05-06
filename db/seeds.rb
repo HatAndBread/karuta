41968160ff6934bae2ce0a7722a4e03eab726236
Joshua Hume <valencelectron@hotmail.com>
2021-05-06 11:32:43 +0900

    quizzes index

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,21 +7,33 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require Rails.root + 'db/dictionary.rb'
 
+def create_questions(quiz)
+    word = dic
+    question = Question.create({content: word[:definition], answer: word[:word], quiz: quiz})
+    puts "New question: #{question.answer}: #{question.content}"
+end
+
+def create_quiz(user)
+    quiz = Quiz.create({name: Faker::Hipster.word, public: [true, false].sample, user: user})
+    puts "Created a new quiz called #{quiz.name} owned by #{quiz.user.email}"
+    quiz
+end
+
 puts "Destroying old database..."
 User.destroy_all
-User.create({email: 'a@a.co', password: 'asdasd'})
+
+me = User.create({email: 'a@a.co', password: 'asdasd'})
+3.times do 
+    quiz = create_quiz(me) 
+    (1..39).to_a.sample.times { create_questions(quiz) }
+end
+
 puts "Old data deleted ✨"
-user = User.create({email: Faker::Internet.email, password: '123456'})
 20.times do 
     user = User.create({email: Faker::Internet.email, password: '123456'})
     puts "Created user: #{user.email}"
     (0..11).to_a.sample.times do
-        quiz = Quiz.create({name: Faker::Hipster.word, public: [true, false].sample, user: user})
-        puts "Created a new quiz called #{quiz.name} owned by #{quiz.user.email}"
-        (1..39).to_a.sample.times do
-            word = dic
-            question = Question.create({content: word[:definition], answer: word[:word], quiz: quiz})
-            puts "New question: #{question.answer}: #{question.content}"
-        end
+        quiz = create_quiz(user)
+        (1..39).to_a.sample.times { create_questions(quiz) }
     end
 end
